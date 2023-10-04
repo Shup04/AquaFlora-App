a0a64c61340344ea0360d3db89272944a83d77df
Shup04 <bradleyschmidt04@gmail.com>
2023-10-04 15:06:32 -0700

    fixed the way graph work, it now adds an entry day

--- Components/ParamChart.js
+++ Components/ParamChart.js
@@ -9,48 +9,103 @@ export const ParamChart = ({ }) => {
   const screenWidth = Dimensions.get('window').width;
   const chartWidth = screenWidth * 0.65;
 
-  const data = [ 
-    {value:50},
-    {value:10}, 
-    {value:40}, 
-    {value:30}, 
-    {value:20}, 
-    {value:60}, 
-    {value:50}, 
-    {value:10}, 
-    {value:40}, 
-    {value:30}, {value:20}, {value:60}]
-  
   const lineData2 = [ 
     { value:30, data: '1 Sept 2023', label: 'Sept', labelTextStyle: {color: 'white', width: 50}}, 
-    { value:15, data: '1 Sept 2023'}, 
-    { value:60, data: '3 Sept 2023'},
-    { value:10, data: '6 Sept 2023'}, 
+    { value:30, data: '2 Sept 2023'}, 
+    { value:15, data: '3 Sept 2023'},
+    { value:15, data: '4 Sept 2023'},
+    { value:15, data: '5 Sept 2023'},
+    { value:15, data: '6 Sept 2023'},
+    { value:15, data: '7 Sept 2023'}, 
+    { value:15, data: '8 Sept 2023'}, 
+    { value:20, data: '9 Sept 2023'},
     { value:20, data: '10 Sept 2023'}, 
-    { value:45, data: '14 Sept 2023'}, 
-    { value:30, data: '19 Sept 2023'}, 
-    { value:35, data: '29 Sept 2023'}, 
+    { value:20, data: '11 Sept 2023'},
+    { value:20, data: '12 Sept 2023'},
+    { value:20, data: '13 Sept 2023'},
+    { value:20, data: '14 Sept 2023'}, 
+    { value:20, data: '15 Sept 2023'}, 
+    { value:25, data: '16 Sept 2023'}, 
+    { value:25, data: '17 Sept 2023'}, 
+    { value:25, data: '18 Sept 2023'}, 
+    { value:25, data: '19 Sept 2023'}, 
+    { value:25, data: '20 Sept 2023'}, 
+    { value:25, data: '21 Sept 2023'}, 
+    { value:25, data: '22 Sept 2023'}, 
+    { value:20, data: '23 Sept 2023'}, 
+    { value:20, data: '24 Sept 2023'}, 
+    { value:20, data: '25 Sept 2023'}, 
+    { value:20, data: '26 Sept 2023'}, 
+    { value:20, data: '27 Sept 2023'}, 
+    { value:20, data: '28 Sept 2023'}, 
+    { value:20, data: '29 Sept 2023'}, 
+    { value:20, data: '30 Sept 2023'}, 
     { value:20, label: 'Oct', labelTextStyle: {color: 'white', width: 50}, data: '1 Oct 2023'}, 
-    { value:10, data: '7 Oct 2023'}, 
-    { value:60, data: '14 Oct 2023'}, 
-    { value:55, data: '23 Oct 2023'}]
+    { value:35, data: '2 Oct 2023'}, 
+    { value:35, data: '3 Oct 2023'}, 
+    { value:35, data: '4 Oct 2023'}, 
+    { value:35, data: '5 Oct 2023'}, 
+    { value:35, data: '6 Oct 2023'}, 
+    { value:35, data: '7 Oct 2023'}, 
+    { value:35, data: '8 Oct 2023'}, 
+    { value:20, data: '9 Oct 2023'}, 
+    { value:20, data: '10 Oct 2023'}
+  ]
+  const data = [ 
+    { value:30, data: '1 Sept 2023', label: 'Sept', labelTextStyle: {color: 'white', width: 50}}, 
+    { value:30, data: '2 Sept 2023'}, 
+    { value:30, data: '3 Sept 2023'},
+    { value:30, data: '4 Sept 2023'},
+    { value:30, data: '5 Sept 2023'},
+    { value:30, data: '6 Sept 2023'},
+    { value:45, data: '7 Sept 2023'}, 
+    { value:45, data: '8 Sept 2023'}, 
+    { value:45, data: '9 Sept 2023'},
+    { value:45, data: '10 Sept 2023'}, 
+    { value:45, data: '11 Sept 2023'},
+    { value:45, data: '12 Sept 2023'},
+    { value:45, data: '13 Sept 2023'},
+    { value:20, data: '14 Sept 2023'}, 
+    { value:20, data: '15 Sept 2023'}, 
+    { value:20, data: '16 Sept 2023'}, 
+    { value:20, data: '17 Sept 2023'}, 
+    { value:20, data: '18 Sept 2023'}, 
+    { value:20, data: '19 Sept 2023'}, 
+    { value:20, data: '20 Sept 2023'}, 
+    { value:35, data: '21 Sept 2023'}, 
+    { value:35, data: '22 Sept 2023'}, 
+    { value:35, data: '23 Sept 2023'}, 
+    { value:35, data: '24 Sept 2023'}, 
+    { value:35, data: '25 Sept 2023'}, 
+    { value:35, data: '26 Sept 2023'}, 
+    { value:35, data: '27 Sept 2023'}, 
+    { value:35, data: '28 Sept 2023'}, 
+    { value:35, data: '29 Sept 2023'}, 
+    { value:55, data: '30 Sept 2023'}, 
+    { value:55, label: 'Oct', labelTextStyle: {color: 'white', width: 50}, data: '1 Oct 2023'}, 
+    { value:55, data: '2 Oct 2023'}, 
+    { value:55, data: '3 Oct 2023'}, 
+    { value:55, data: '4 Oct 2023'}, 
+    { value:55, data: '5 Oct 2023'}, 
+    { value:55, data: '6 Oct 2023'}, 
+    { value:20, data: '7 Oct 2023'}, 
+    { value:20, data: '8 Oct 2023'}, 
+    { value:20, data: '9 Oct 2023'}, 
+    { value:20, data: '10 Oct 2023'}
+  ]
   return (
     <LineChart
             areaChart
             curved
             rotateLabel
-            data={lineData2}
-            //data2={lineData2}
+            data={data}
+            data2={lineData2}
             height={200}
-            showVerticalLines
             spacing={chartWidth / data.length}
             initialSpacing={10}
             color1="red"
             color2="orange"
-            textColor1="green"
             hideDataPoints
-            dataPointsColor1="red"
-            dataPointsColor2="red"
             startFillColor1="red"
             startFillColor2="orange"
             endFillColor1="red"
@@ -61,9 +116,9 @@ export const ParamChart = ({ }) => {
             yAxisColor="white"
             yAxisTextStyle={{color: 'white'}}
             rulesType='solid'
-            rulesColor={"#EEEEEE33"}
+            rulesColor={"#EEEEEE55"}
             verticalLinesColor={"#EEEEEE33"}
-            
+            noOfSections={5}
             />
   );
 };
