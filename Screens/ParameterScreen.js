bbdb334725f3a32bc19c674df344f537a99400a9
shup04 <bradleyschmidt04@gmail.com>
2024-01-11 22:53:42 -0800

    parameter graph is looking mostly nice

--- Screens/ParameterScreen.js
+++ Screens/ParameterScreen.js
@@ -60,34 +60,97 @@ const data = [
     { value:20, data: '9 Oct 2023'}, 
     { value:20, data: '10 OC 2023'}
   ]
+  const ptData = [
+    {value: 16, date: '1 Apr 2022'},
+    {value: 18, date: '2 Apr 2022'},
+    {value: 19, date: '3 Apr 2022'},
+    {value: 18, date: '4 Apr 2022'},
+    {value: 14, date: '5 Apr 2022'},
+    {value: 14, date: '6 Apr 2022'},
+    {value: 16, date: '7 Apr 2022'},
+    {value: 20, date: '8 Apr 2022'},
+  
+    {value: 22, date: '9 Apr 2022'},
+    {
+      value: 24,
+      date: '10 Apr 2022',
+      label: '10 Apr',
+      labelTextStyle: {color: 'lightgray', width: 60},
+    },
+    {value: 28, date: '11 Apr 2022'},
+    {value: 26, date: '12 Apr 2022'},
+    {value: 34, date: '13 Apr 2022'},
+    {value: 38, date: '14 Apr 2022'},
+    {value: 28, date: '15 Apr 2022'},
+    {value: 39, date: '16 Apr 2022'},
+  
+    {value: 37, date: '17 Apr 2022'},
+    {value: 28, date: '18 Apr 2022'},
+    {value: 29, date: '19 Apr 2022'},
+    {
+      value: 30,
+      date: '20 Apr 2022',
+      label: '20 Apr',
+      labelTextStyle: {color: 'lightgray', width: 60},
+    },
+    {value: 28, date: '21 Apr 2022'},
+    {value: 29, date: '22 Apr 2022'},
+    {value: 26, date: '23 Apr 2022'},
+    {value: 25, date: '24 Apr 2022'},
+  
+    {value: 19, date: '25 Apr 2022'},
+    {value: 22, date: '26 Apr 2022'},
+    {value: 20, date: '27 Apr 2022'},
+    {value: 23, date: '28 Apr 2022'},
+    {value: 21, date: '29 Apr 2022'},
+    {
+      value: 20,
+      date: '30 Apr 2022',
+      label: '30 Apr',
+      labelTextStyle: {color: 'lightgray', width: 60},
+    },
+    {value: 24, date: '1 May 2022'},
+    {value: 25, date: '2 May 2022'},
+    {value: 28, date: '3 May 2022'},
+    {value: 25, date: '4 May 2022'},
+    {value: 21, date: '5 May 2022'},
+  ];
 
   return (
     <View style={styles.body}>
-      <LineChart
-          areaChart
-          isAnimated
-          data={data}
-          //data2={lineData2}
-          //height={85}
-          width={chartWidth}
-          spacing={chartWidth/data.length}
-          endSpacing={0}
-          initialSpacing={0}
+        <View style={styles.container}>
+        <LineChart
+            areaChart
+            isAnimated
+            data={data}
+            data2={ptData}
+            height={400}
+            width={chartWidth}
+            spacing={20}
+            endSpacing={0}
+            initialSpacing={0}
 
+            color1="#8a56ce"
+            color2="#56acce"
+            startFillColor1="#8a56ce"
+            startFillColor2="#56acce"
+            endFillColor1="#8a56ce"
+            endFillColor2="#56acce"
 
-
-          hideDataPoints
-          startOpacity={0.5}
-          endOpacity={0.1}
-          xAxisColor={Colors.textMarine}
-          yAxisColor={Colors.textMarine}
-          yAxisTextStyle={{color: 'white'}}
-          rulesType='solid'
-          rulesColor={"#EEEEEE55"}
-          verticalLinesColor={"#EEEEEE33"}
-          maxValue={60}
-          noOfSections={2}
-        />
+            hideDataPoints
+            startOpacity={0.5}
+            endOpacity={0.1}
+            xAxisColor={'lightgray'}
+            
+            yAxisThickness={0}
+            yAxisTextStyle={{color: 'white'}}
+            rulesType='solid'
+            rulesColor={"#EEEEEE55"}
+            verticalLinesColor={"#EEEEEE33"}
+            maxValue={100}
+            noOfSections={4}
+            />
+        </View>
     </View>
   );
 };
@@ -104,9 +167,9 @@ const styles = StyleSheet.create({
     },
     container: {
       width: '100%',
-      height: '100%',
-      paddingTop: 30,
-      //alignItems: 'center',
+      //marginBottom: 30,
+      marginTop: 50,
+
     },
     scrollContainer: {
       flexGrow: 1,
