2d95db71e94874dcbdc1bb9f2226d909066946ab
shup04 <bradleyschmidt04@gmail.com>
2024-02-20 01:25:46 -0800

    small param chart obeys tankId

--- Components/ParamChart.js
+++ Components/ParamChart.js
@@ -18,91 +18,6 @@ export const ParamChart = ({ navigation, tankId }) => {
   const [Nitrite, setNitrite] = useState([]);
   const [Ph, setPh] = useState([]);
 
-  const lineData2 = [ 
-    { value:30, data: '1 Sept 2023', label: 'Sept', labelTextStyle: {color: 'white', width: 50}}, 
-    { value:30, data: '2 Sept 2023'}, 
-    { value:15, data: '3 Sept 2023'},
-    { value:15, data: '4 Sept 2023'},
-    { value:15, data: '5 Sept 2023'},
-    { value:15, data: '6 Sept 2023'},
-    { value:15, data: '7 Sept 2023'}, 
-    { value:15, data: '8 Sept 2023'}, 
-    { value:20, data: '9 Sept 2023'},
-    { value:20, data: '10 Sept 2023'}, 
-    { value:20, data: '11 Sept 2023'},
-    { value:20, data: '12 Sept 2023'},
-    { value:20, data: '13 Sept 2023'},
-    { value:20, data: '14 Sept 2023'}, 
-    { value:20, data: '15 Sept 2023'}, 
-    { value:25, data: '16 Sept 2023'}, 
-    { value:25, data: '17 Sept 2023'}, 
-    { value:25, data: '18 Sept 2023'}, 
-    { value:25, data: '19 Sept 2023'}, 
-    { value:25, data: '20 Sept 2023'}, 
-    { value:25, data: '21 Sept 2023'}, 
-    { value:25, data: '22 Sept 2023'}, 
-    { value:20, data: '23 Sept 2023'}, 
-    { value:20, data: '24 Sept 2023'}, 
-    { value:20, data: '25 Sept 2023'}, 
-    { value:20, data: '26 Sept 2023'}, 
-    { value:20, data: '27 Sept 2023'}, 
-    { value:20, data: '28 Sept 2023'}, 
-    { value:20, data: '29 Sept 2023'}, 
-    { value:20, data: '30 Sept 2023'}, 
-    { value:20, label: 'Oct', labelTextStyle: {color: 'white', width: 50}, data: '1 Oct 2023'}, 
-    { value:35, data: '2 Oct 2023'}, 
-    { value:35, data: '3 Oct 2023'}, 
-    { value:35, data: '4 Oct 2023'}, 
-    { value:35, data: '5 Oct 2023'}, 
-    { value:35, data: '6 Oct 2023'}, 
-    { value:35, data: '7 Oct 2023'}, 
-    { value:35, data: '8 Oct 2023'}, 
-    { value:20, data: '9 Oct 2023'}, 
-    { value:20, data: '10 Oct 2023'}
-  ]
-  const data = [ 
-    { value:30, data: '1 Sept 2023', label: 'Sept', labelTextStyle: {color: 'white', width: 50}}, 
-    { value:30, data: '2 Sept 2023'}, 
-    { value:30, data: '3 Sept 2023'},
-    { value:30, data: '4 Sept 2023'},
-    { value:30, data: '5 Sept 2023'},
-    { value:30, data: '6 Sept 2023'},
-    { value:45, data: '7 Sept 2023'}, 
-    { value:45, data: '8 Sept 2023'}, 
-    { value:45, data: '9 Sept 2023'},
-    { value:45, data: '10 Sept 2023'}, 
-    { value:45, data: '11 Sept 2023'},
-    { value:45, data: '12 Sept 2023'},
-    { value:45, data: '13 Sept 2023'},
-    { value:20, data: '14 Sept 2023'}, 
-    { value:20, data: '15 Sept 2023'}, 
-    { value:20, data: '16 Sept 2023'}, 
-    { value:20, data: '17 Sept 2023'}, 
-    { value:20, data: '18 Sept 2023'}, 
-    { value:20, data: '19 Sept 2023'}, 
-    { value:20, data: '20 Sept 2023'}, 
-    { value:35, data: '21 Sept 2023'}, 
-    { value:35, data: '22 Sept 2023'}, 
-    { value:35, data: '23 Sept 2023'}, 
-    { value:35, data: '24 Sept 2023'}, 
-    { value:35, data: '25 Sept 2023'}, 
-    { value:35, data: '26 Sept 2023'}, 
-    { value:35, data: '27 Sept 2023'}, 
-    { value:35, data: '28 Sept 2023'}, 
-    { value:35, data: '29 Sept 2023'}, 
-    { value:55, data: '30 Sept 2023'}, 
-    { value:55, label: 'Oct', labelTextStyle: {color: 'white', width: 50}, data: '1 Oct 2023'}, 
-    { value:55, data: '2 Oct 2023'}, 
-    { value:55, data: '3 Oct 2023'}, 
-    { value:55, data: '4 Oct 2023'}, 
-    { value:55, data: '5 Oct 2023'}, 
-    { value:55, data: '6 Oct 2023'}, 
-    { value:20, data: '7 Oct 2023'}, 
-    { value:20, data: '8 Oct 2023'}, 
-    { value:20, data: '9 Oct 2023'}, 
-    { value:20, data: '10 OC 2023'}
-  ]
-
   const parameters = [
     { color: '#ff8d4f', text: 'Nitrate' },
     { color: '#47fcd8', text: 'Nitrite' },
@@ -112,7 +27,7 @@ export const ParamChart = ({ navigation, tankId }) => {
 
   useEffect(() => {
 
-    const allParams = realm.objects('WaterParameter');
+    const allParams = realm.objects('WaterParameter').filtered(`tankId = ${tankId}`);
     const data1 = setupData(allParams).finalData1;
     const data2 = setupData(allParams).finalData2;
     const data3 = setupData(allParams).finalData3;
