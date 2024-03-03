25e8b3695d3a1f3a11f1554d2a52d26c8353a30d
shup04 <bradleyschmidt04@gmail.com>
2024-03-03 02:16:51 -0800

    started fish add modal

--- Homepage Content/FishContent.js
+++ Homepage Content/FishContent.js
@@ -1,8 +1,9 @@
 import  React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Modal, ScrollView, Image } from 'react-native';
 import { FishComponent } from '../Components/FishItem';
 import data from '../Data/fish_data.json';
 import { Colors } from '../Colors';
+import { BlurView } from 'expo-blur';
 
 export const FishContent = ({ navigation }) => {
 
@@ -61,19 +62,47 @@ export const PersonalFishContent = ({ navigation }) => {
 
 export const PlusComponent = ({ navigation, destination, tankId }) => {
 
-  const handlePress = (tankId) => {
-    navigation.navigate(destination, {tankId: tankId});
+  const [fishPick, setFishPick] = useState(false);
+
+  const handlePress = () => {
+    setFishPick(true);
+  }; 
+  const handleCloseModal = () => {
+    setFishPick(false);
   }; 
 
   return (
-  <View style={styles.plusButtonContainer}>
-    <TouchableOpacity 
-      style={styles.plusButton}
-      onPress={() => handlePress(tankId)}
-    >
-      <Text style={styles.plusButtonText}>Add New Fish</Text>
-    </TouchableOpacity>
-  </View>
+    <>
+      <View style={styles.plusButtonContainer}>
+        <TouchableOpacity 
+          style={styles.plusButton}
+          onPress={() => handlePress()}
+        >
+          <Text style={styles.plusButtonText}>Add New Fish</Text>
+        </TouchableOpacity>
+      </View>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={fishPick}
+        onRequestClose={handleCloseModal}
+      >
+        <View style={modalStyles.KeyboardAvoidingView}>
+        <BlurView style={modalStyles.centeredView} tint={'dark'}>
+          <View style={modalStyles.modalView}>
+            <View style={{ borderRadius: 8, overflow: 'hidden' }}>
+              <ScrollView>
+                
+                <Text style={modalStyles.modalTitle}>"hi"</Text>
+                <Text style={modalStyles.modalSubtitle}>"Hi"</Text>
+                <Text style={modalStyles.modalText}>"hi"</Text>
+              </ScrollView>
+            </View>
+          </View>
+        </BlurView>
+        </View>
+      </Modal>
+    </>
   );
 };
 
@@ -112,4 +141,73 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },  
+});
+
+const modalStyles = StyleSheet.create({
+  centeredView: {
+    height: '100%',
+    marginTop: -32,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#FFFFFF',
+  },
+  modalView: {
+    height: '50%',
+    width: '80%',
+    margin: 20,
+    backgroundColor: Colors.height3,
+    //backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    padding: 35,
+    //alignItems: "center",
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.textMarine,
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 15,
+    //textAlign: "center",
+    color: Colors.textMarine,
+  },
+  button: {
+    borderRadius: 20,
+    elevation: 2,
+    backgroundColor: Colors.height4,
+    width: '50%',
+    height: '70%',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    //marginBottom: 5,
+    //marginTop: 'auto',
+  },
+  buttonText: {
+    alignSelf: 'center',
+    fontSize: 16,
+    color: Colors.textMarine,
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    height: '20%',
+    //justifyContent: 'flex-end',
+    alignItems: 'center',
+    //backgroundColor: 'green',
+    marginBottom: 0,
+    marginTop: 'auto',
+  },
 });
