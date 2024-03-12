e9caebf6af62527f10f916ec72a768327db3a281
shup04 <bradleyschmidt04@gmail.com>
2024-03-12 01:32:28 -0700

    slight tabbar styles

--- Core/TabBar.js
+++ Core/TabBar.js
@@ -4,11 +4,33 @@ import { FishScreen } from '../Screens/HomeScreens/Fish';
 import { PlantsScreen } from '../Screens/HomeScreens/Plants';
 import { TanksScreen } from '../Screens/HomeScreens/Tanks';
 
+import { Colors } from '../Colors';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+
 const Tab = createBottomTabNavigator();
 
 export const HomeTabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator 
+    screenOptions={{
+        tabBarActiveTintColor: Colors.primary, // Active icon color
+        tabBarInactiveTintColor: Colors.secondary, // Inactive icon color
+        tabBarStyle: {
+          backgroundColor: Colors.height2, // Tab bar background color
+          borderTopColor: 'transparent', // Hide the top border of the tab bar
+          paddingBottom: 5,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12, // Adjust the size as needed
+          fontWeight: 'bold',
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: Colors.primary, // Color of the indicator (underline)
+        },
+        headerShown: false, // Optionally hide the header
+      }}
+      >
       <Tab.Screen name="Dashboard" component={DashboardScreen} />
       <Tab.Screen name="Tanks" component={TanksScreen} />
       <Tab.Screen name="Fish" component={FishScreen} />
